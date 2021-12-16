8656919a1b3db9f8466079e8451c932b2202120b
Dora <939217129@qq.com>
2021-12-16 15:21:05 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div>
+        <div>您查找的页面不存在，请查询网址是否正确</div>
+        <div>
+            <router-link to="/">返回首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NotFound'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
