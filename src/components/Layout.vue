06893ae00fd8b396eb9dbefc7ac55fe08477ff1f
Dora <939217129@qq.com>
2021-12-23 10:38:05 +0800

    修改了css和html

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,29 +1,27 @@
 <template>
-    <div class="nav-wrapper">
-        <div class="content">
-            <slot/>
-        </div>
-        <Nav/>
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`   ">
+      <slot/>
     </div>
+    <Nav/>
+  </div>
 </template>
 
 <script>
     export default {
-        name: 'Layout'
+        name: 'Layout',
+        props: ['classPrefix']
     }
 </script>
 
 <style lang="scss" scoped>
-    .nav-wrapper{
-        border: 1px solid green;
+    .layout-wrapper{
         display: flex;
         flex-direction: column;
         height: 100vh;
-        
         }
     .content{
         flex-grow: 1; 
         overflow: auto;
     }
-
 </style>
