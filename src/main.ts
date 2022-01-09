7667322513436c4564197fd9b100dba8996834a1
Dora <939217129@qq.com>
2022-01-09 16:03:23 +0800

    将所有数据封装在store里

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import Nav from '@/components/Nav.vue'
-import Layout from '@/components/Layout.vue'
-import Icon from '@/components/Icon.vue'
-import tagListModel from '@/models/tagListModel';
-import recordListModel from '@/models/recordListModel';
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Nav from '@/components/Nav.vue';
+import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.component('Nav', Nav)
-Vue.component('Layout', Layout)
-Vue.component('Icon', Icon)
-
-// record store
-window.recordList = recordListModel.fetch();
-
-window.createRecord = (record: RecordItem) => recordListModel.create(record);
+Vue.component('Nav', Nav);
+Vue.component('Layout', Layout);
+Vue.component('Icon', Icon);
 
 
-// tag store
-window.tagList = tagListModel.fetch();
-
-window.findTag = (id: string) => {
-  return window.tagList.filter(t => t.id === id)[0];
-}
-
-window.createTag = (name: string) => {
-  const message = tagListModel.create(name);
-  if (message === 'duplicate') {
-    window.alert('标签名重复');
-  } else if (message === 'success') {
-    window.alert('标签名创建成功');
-  }
-}
-
-window.removeTag = (id: string) => {
-  return tagListModel.remove(id);
-}
-
-
-window.updateTag = (id: string, name: string) => {
-  return tagListModel.update(id, name);
-}
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
